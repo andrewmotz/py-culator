3e6b488cc77377bb643a5e40e1526f7e78aab6c6
Andrew Motz <andrewmozza555@gmail.com>
2023-02-11 17:54:10 -0800

    Move into main function

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-print("Hello world! :)")
 
-print("Hello world #2")
-
+# This does subtraction
 def subtraction(a ,b):
     return a - b
+
+def main():
+    print("Hello world! :)")
+    print("Hello world #2")
